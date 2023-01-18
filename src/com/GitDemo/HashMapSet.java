56d319d9d5e645c90c7c60c27ec744c55470457b
nileema <nileemabarwal7@gmail.com>
2023-01-18 22:48:41 +0530

    New project

---
+++ src/com/GitDemo/HashMapSet.java
@@ -0,0 +1,17 @@
+package com.GitDemo;
+
+import java.util.HashMap;
+
+public class HashMapSet {
+
+	public static void main(String[] args) {
+		
+		HashMap hs=new HashMap();
+		hs.put("Nilu", 10);
+		hs.put("golu", 20);
+		hs.put("molu", 30);
+		System.out.println(hs);
+		
+	}
+
+}
---
+++ src/com/GitDemo/HashSetDemo.java
@@ -0,0 +1,18 @@
+package com.GitDemo;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		 
+		HashSet<Integer> set=new HashSet<Integer>();
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		System.out.println(set);
+		
+	}
+
+}
